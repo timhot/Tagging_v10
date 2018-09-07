f64a6432e25ad89e37a04a06b0b7687277a0aa35
timhot <timhot@hotmail.com>
2018-09-07 14:06:42 +1200

    1

---
+++ src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="login">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data () {
+      return {
+        msg: 'Enter your username'
+      }
+    }
+  }
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/Home'
+import Login from '@/components/Login'
+import About from '@/components/About'
+import Coins from '@/components/Coins'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'About',
+      component: About
+    },
+    {
+      path: '/coins/:id',
+      name: 'Coins',
+      component: Coins
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+
+  ]
+})
